1dcc9a2d20c5240315240c455a28d6af4a6f9f63
badr ghanbiE <bghaE@gmail.com>
2022-10-12 18:17:14 +0100

    add the body background image when its daytime

--- App/Js/main.js
+++ App/Js/main.js
@@ -1,14 +1,23 @@
-
-
+import {a} from './Quoates.js';
+import { sum } from './Quoates.js';
 const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 
 
-const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+// const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
 
 let Quoates="https://api.quotable.io/random";
- let timeApi="https://worldtimeapi.org/api/ip/";
+let timeApi="https://worldtimeapi.org/api/ip/";
+ async function getData(url){
+  let res= await fetch(url);
+  let data=await res.json();
+  console.log(data)
+ }
 
-fetch(timeApi)
-.then(res=>res.json())
-.then(data=>console.log(data))
+    
+console.log(getData(Quoates))
+
+console.log(a)
+
+
+console.log(sum(2,9))
