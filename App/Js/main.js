081ab0a02ee4c679754cb3688d43d62a0c61c135
badr ghanbiE <bghaE@gmail.com>
2022-10-13 13:15:22 +0100

    the region first div element styles and make it responsive

--- App/Js/main.js
+++ App/Js/main.js
@@ -1,14 +1,21 @@
  import {generateQuote,QuoateContainer} from './Quoates.js';
-const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 let timeApi="https://worldtimeapi.org/api/ip/";
 
 // Generate a Quoates each time the browser got refreshed our when visiting the page at the first time
  generateQuote();
- 
+
 // let's Generate a new quote for the user when he choice to generate another one
  QuoateContainer.addEventListener('click',(e)=>{
     (e.target.className==="quote__refresh") ? generateQuote():"";
  })
 
+//  =====> timeApi
 
+// lets get data from the time Api
+ async function getTimeAndCountry(){
+        let res= await fetch(timeApi);
+        let data=await res.json();
+       console.log(data)
+} 
 
+getTimeAndCountry()
